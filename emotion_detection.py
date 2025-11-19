a8514be52b88128adf8182233a63e645cad8fc3f
Your Name <you@example.com>
2025-11-19 14:27:42 -0500

    T

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,9 @@
 import requests  
 
-def emotion_detector():
-    URL: 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    Headers: {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    myobj: { "raw_document": { "text": text_to_analyse } }
-    response =requests.post(URL, json = myobj, headers=Headers)
+def emotion_detector(text_to_analyse):
+    URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    myobj = { "raw_document": { "text": text_to_analyse } }
+    Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    
+    response = requests.post(URL, json=myobj, headers=Headers)
     return response.text
